d3a5feff0cfd2ef010ee72ad71899cb06a79a85b
carollia99 <carollia@usc.edu>
2020-11-02 04:40:49 -0500

    set up starter code for react and connect to server + database

---
+++ development/api/server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const {MongoClient} = require('mongodb');
+
+var cors = require('cors');
+const app = express();
+
+const password = 'Carol1234';
+const uri = `mongodb+srv://carol:${password}@cluster0.zyzjm.mongodb.net/?retryWrites=true&w=majority`;
+const mongo = new MongoClient(uri);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//connect to mongodb database
+mongo.connect(function (err) {
+    if (!err) {
+        console.log("successfully connected to database!" );
+    } else {
+        console.log("error connecting to database.");
+    }
+
+    const db = mongo.db('rso_board');
+
+    // write your endpoints for getting + adding data here. the below is an example retrieving all objects from Users collection
+    app.get('/allUsers', (request, response) => {
+       db.collection('Users').find({})
+           .toArray()
+           .then((result) => {
+               response.status(200).json(result)
+           })
+           .catch((error) => {
+               response.status(400).send(error.message);
+           })
+    });
+
+});
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(4000, function () {
+    console.log('Listening on port 4000!');
+});
