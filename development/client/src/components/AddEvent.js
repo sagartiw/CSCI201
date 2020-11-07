2445a39e31a1ffcc0661ca5e86067867387b4863
sagartiw <sagartiw@usc.edu>
2020-11-06 17:46:02 -0800

    standardized components

---
+++ development/client/src/components/AddEvent.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const AddEvent = () => {
+    return(
+        <div>
+            <h1>Add it.</h1>
+        </div>
+    )
+}
---
+++ development/client/src/components/EditEvent.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const EditEvent = () => {
+    return(
+        <div>
+            <h1>Edit it</h1>
+        </div>
+    )
+}
