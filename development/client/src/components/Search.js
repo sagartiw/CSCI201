0f627ecc7b0745db2f398bd0859f4ae99d57737b
Albert Tan <alberthtan@Alberts-MacBook-Air.local>
2020-11-23 21:25:18 -0800

    no u

--- development/client/src/components/Search.js
+++ development/client/src/components/Search.js
@@ -37,6 +37,25 @@ export const SearchPanel = () =>{
                     <Button onClick={loadEvents}>Enter</Button>
                 </Form>
             </div>
+
+            <ListGroup className="mt-4">
+                {allEvents.length > 0 ? (
+                    <>
+                        {allEvents.map((allEvents) => (
+                            <ListGroupItem className="d-flex" key={allEvents._id}>
+                                <strong>{allEvents.name}</strong>
+                                <div className="ml-auto">
+                                    <Link to={`/DetailEvent/${allEvents._id}`} color="success" className="btn btn-success mr-1">Details</Link>
+                                    <Link to={`/EditEvent/${allEvents._id}`} color="warning" className="btn btn-warning mr-1">Edit</Link>
+                                    <Button onClick={() => removeEvent(allEvents._id)} color="danger">Delete</Button>
+                                </div>
+                            </ListGroupItem>
+                        ))}
+                    </>
+                ) : (
+                    <h4 className="text-center">No Events</h4>
+                )}
+            </ListGroup>
         </>
     )
 }
--- development/client/src/pages/Home.js
+++ development/client/src/pages/Home.js
@@ -2,8 +2,10 @@ import React from 'react';
 import {NavbarPanel} from '../components/Navbar';
 import {EventsPanel} from '../components/Events';
 import {SearchPanel} from "../components/Search";
+import axios from "axios";
 
 export const Home = () => {
+
     return(
         <div>
             <NavbarPanel/>
