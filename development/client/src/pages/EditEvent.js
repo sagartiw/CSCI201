4c64977558e89e5a5e2df2c021b14cabd862ccc9
Akhilesh Bellathur <abellath@usc.edu>
2020-11-24 00:03:21 -0800

    EditEvent.js functionality works, but backend call doesn't

--- development/client/src/pages/EditEvent.js
+++ development/client/src/pages/EditEvent.js
@@ -6,44 +6,103 @@ import {
     FormGroup,
     Label,
     Input,
-    Button
+    Button, ListGroupItem, ListGroup
 } from 'reactstrap';
 import {NavbarPanel} from "../components/Navbar";
+import axios from "axios";
 
 export const EditEvent = (props) => {
-    const [selectedEvent, setSelectedEvent] = useState({
-        id: '',
-        name: ''
-    });
+    const [name, setName] = useState('');
+    const [organization, setOrganization] = useState('');
+    const [time, setTime] = useState('');
+    const [desc, setDescription] = useState('');
+    const [selectedEvent, setSelectedEvent] = useState('');
     const{ events, editEvent } = useContext(GlobalContext);
     const history = useHistory();
-    const currentEventId = props.match.params.id;
+    const currentEventName = props.match.params.name;
 
     useEffect(() => {
-        const eventId = currentEventId;
-        const selectedEvent = events.find(event => event.id === Number(eventId))
-        setSelectedEvent(selectedEvent)
-    }, [currentEventId, events])
+        let url = "http://localhost:4000/getEvent?name=" + currentEventName;
+        axios.get(url)
+        .then(function (response) {
+            console.log(response.data);
+            setSelectedEvent(response.data);
+        })
+    }, [currentEventName, events])
 
-    const onSubmit = () => {
-        editEvent(selectedEvent);
-        history.push("/");
+    const nameChange = (e) =>{
+        setName(e.target.value);
     }
 
-    const onChange = (e) =>{
-        setSelectedEvent({...selectedEvent, [e.target.name]: e.target.value})
+    const organizationChange = (e) =>{
+        setOrganization(e.target.value);
+    }
+
+    const timeChange = (e) =>{
+        setTime(e.target.value);
+    }
+
+    const descChange = (e) =>{
+        setDescription(e.target.value);
+    }
+
+    //Somehow adds a new event instead of properly editing.
+    const onSubmit = () => {
+        let url = "http://localhost:4000/editEvent";
+        axios.post(url, {
+            organization: organization,
+            time: new Date(time),
+            name: name,
+            keywords: name,
+            description: desc
+        })
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+        console.log("FINISHED SUBMITTING")
+        history.push("/");
     }
 
     return(
         <div>
             <NavbarPanel/>
             <Form onSubmit={onSubmit} style={{maxWidth: "30rem", padding:"1rem"}}>
-                <FormGroup>
-                    <Label>Name</Label>
-                    <Input type="text" name="name" value={selectedEvent.name} onChange={onChange} placeholder="Enter Name"></Input>
-                </FormGroup>
-                <Button type="submit">Edit Name</Button>
-                <Link to="/" className="btn btn-danger ml-2">Cancel</Link>
+                    {selectedEvent.length > 0 ? (
+                        <>
+                            {selectedEvent.map((selectedEvent) => (
+                                <FormGroup>
+                                    <Label>Name</Label>
+                                    <Input type="text" value={name} onChange={nameChange} placeholder={selectedEvent.name}></Input>
+                                </FormGroup>
+                            ))}
+                            {selectedEvent.map((selectedEvent) => (
+                                <FormGroup>
+                                    <Label>Organization</Label>
+                                    <Input type="text" name="Organization" value={organization} onChange={organizationChange} placeholder={selectedEvent.organization}></Input>
+                                </FormGroup>
+                            ))}
+                            {selectedEvent.map((selectedEvent) => (
+                                <FormGroup>
+                                    <Label>Time</Label>
+                                    <Input type="text" name="Time" value={time} onChange={timeChange} placeholder={selectedEvent.time}></Input>
+                                </FormGroup>
+                            ))}
+                            {selectedEvent.map((selectedEvent) => (
+                                <FormGroup>
+                                    <Label>Description</Label>
+                                    <Input type="text" name="description" value={desc} onChange={descChange} placeholder={selectedEvent.description}></Input>
+                                </FormGroup>
+                            ))}
+
+                            <Button type="submit">Submit</Button>
+                            <Link to="/" className="btn btn-danger ml-2">Cancel</Link>
+                        </>
+                    ) : (
+                        <h4 className="text-center">Error finding event :(</h4>
+                    )}
             </Form>
         </div>
     )
