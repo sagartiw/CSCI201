216d03579cfcce0a49c633a92d22e5f4b486bf4f
sagartiw <sagartiw@usc.edu>
2020-11-23 23:20:43 -0600

    slight naming changes

--- development/client/src/pages/Home.js
+++ development/client/src/pages/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {NavbarPanel} from '../components/Navbar';
-import {EventsPanel} from '../components/EventsPanel';
+import {EventsPanel} from '../components/Events';
 import {SearchPanel} from "../components/Search";
 
 export const Home = () => {
