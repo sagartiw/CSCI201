5dcbcac14f51b2031eba834e8f85fc2381fd3798
Albert Tan <alberthtan@Alberts-MacBook-Air.local>
2020-11-23 22:35:24 -0800

    changed _id to name for events

--- development/client/src/App.js
+++ development/client/src/App.js
@@ -18,12 +18,12 @@ function App() {
                   <Switch>
                       <Route path ='/Login' component={Login} />
                       <Route path ='/AddEvent' component={AddEvent} />
-                      <Route path ='/EditEvent/:name' component={EditEvent}/>
+                      <Route path ='/EditEvent/:_id' component={EditEvent}/>
                       <Route path ='/' exact component={Home} />
                       <Route path ='/Home' component={Home} />
                       <Route path ='/Profile' component={Profile} />
                       <Route path ='/EditProfile' component={EditProfile} />
-                      <Route path ='/DetailEvent/:name' component={DetailEvent}/>
+                      <Route path ='/DetailEvent/:_id' component={DetailEvent}/>
                   </Switch>
               </Router>
           </GlobalProvider>
